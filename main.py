479df153f32a2eae3251136fb96dbffeb844370f
Jimmy Isaac Ventura Salcedo <jimmy.ventura@gmail.com>
2022-01-05 13:48:34 +0000

    Finished Day 1 coding journey

--- main.py
+++ main.py
@@ -1,2 +1,46 @@
 print("hello Jimmy")
 #Code idea, add a little subroutine that captures an error and googles the text of the error.
+print("Day 1 - what to print function")
+print("the function is declared like this")
+print("print('what i want to print')")
+print("you can escape double quotes with \\")
+print("print(\"what i want to print\")")
+print("all this in one single print line using \\n like this: \nDay 1 - what to print function\nthe function is declared like this")
+print("combine"+"three"+"string")
+print("print"+" "+"strings"+" "+"separated")
+#make a code that intentionally fails due to identation
+
+#Fix the code below 👇
+
+#p#rint(Day 1 - String Manipulation")
+p#rint("String Concatenation is done with the "+" sign.")
+ # print('e.g. print("Hello " + "world")')
+p#rint(("New lines can be created with a backslash and n.")
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")) 
+name=input("what is your name?")
+print("your name has "+str(len(name))+ " characters")
+name=input("no, really what is your real name?")
+print("your name is "+name)
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+c=a
+a=b
+b=c
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
